4b1ee68da41f381738d62ad9266c49a23ce7c7d9
ArzateOscar33 <oscar.arzate201@tectijuana.edu.mx>
2023-05-31 06:57:21 -0700

    Se agrega validacion a login

---
+++ src/Components/Pages/Alert.js
@@ -0,0 +1,10 @@
+export function Alert({message}){
+    return(
+        <div
+        className=""
+        role="alert"
+        >
+        <span className="">{message}</span>
+    </div>
+    )
+}
--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { useState } from 'react';
 import { useAuth } from '../../Context/authContext';
 import { useNavigate } from 'react-router-dom';
-
+import Swal from 'sweetalert2';
+import { Alert } from './Alert';
 export const Login = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState({
@@ -15,17 +16,39 @@ export const Login = () => {
   const handleChange = ({ target: { name, value } }) => {
     setUser({ ...user, [name]: value });
   };
-
-  const handleSubmit = e => {
+const [error,setError]=useState();
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("")
+    
     const { email, password } = user;
     const vacios = email.length === 0 || password === 0;
+    try{
+      
     if (!vacios) {
-      login(user.email, user.password);
+      await login(user.email, user.password);
       navigate('/');
     } else {
       // Aquí puedes agregar una lógica para manejar campos vacíos
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Ingrese todos los datos',
+        showConfirmButton: false,
+        timer: 1500
+      });
     }
+  }catch(error){
+     // Aquí puedes agregar una lógica para manejar campos incorrectos
+    // setError(error.message)
+     Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: 'Usuario o Contraseña Incorrectos',
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
   };
 
   const handleCreateAccount = () => {
@@ -42,6 +65,7 @@ export const Login = () => {
 
   return (
     <div className="container bg-gray">
+      {error && <Alert message={error}/>}
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="card bg-primary text-white">
